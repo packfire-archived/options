00898ad986c25466f6bffb2f41c78a8ff7d785c7
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-06 23:43:56 +0800

    version reset to 1.0.0 for classes Since attribute

--- src/Packfire/Command/IOption.php
+++ src/Packfire/Command/IOption.php
@@ -10,7 +10,7 @@ namespace Packfire\Command;
  * @copyright Copyright (c) 2010-2012, Sam-Mauris Yong
  * @license http://www.opensource.org/licenses/bsd-license New BSD License
  * @package Packfire\Command
- * @since 2.0.0
+ * @since 1.0.0
  */
 interface IOption {
     
