36b611043051f2db672bf098f790438a87853d30
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-14 05:48:59 +0800

    licensing updates

--- src/Packfire/Command/Option.php
+++ src/Packfire/Command/Option.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Packfire Options
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
 namespace Packfire\Command;
 
 /**
